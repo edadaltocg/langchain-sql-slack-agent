22f066460442454d13a37248cabd26d59f005029
Eduardo Dadalto <edadaltocg@gmail.com>
2024-05-07 16:52:50 +0200

    langchain server

---
+++ langchain-app-server/app/server.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langserve import add_routes
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Set all CORS enabled origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+add_routes(
+    app,
+    ChatOpenAI(),
+    path="/openai",
+)
+
+model = ChatOpenAI()
+prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
+add_routes(
+    app,
+    prompt | model,
+    path="/joke",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
